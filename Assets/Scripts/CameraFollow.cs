3e2613a9b6351d86a237a29a362100232d3cc3b3
Rachit Goyal <91666319+Rachit8126@users.noreply.github.com>
2022-05-10 15:12:07 +0530

    First Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Component player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = player.transform.position.x - 6f;
+        pos.y = player.transform.position.y + 6f;
+        pos.z = player.transform.position.z;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 50f;
+
+    string GroundTag = "Level";
+    string CollectTag = "gem";
+
+    private Rigidbody rb;
+    bool isGround = true;
+
+    private int score = 25;
+
+    public TextMeshProUGUI rem, win, info;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        info.text = SystemInfo.deviceUniqueIdentifier;
+
+        float z = Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(x * speed * Time.deltaTime, 0, -z * speed * Time.deltaTime);
+
+        rb.AddForce(force);
+
+        if(transform.position.y < -20)
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            rb.AddForce(0, 400f, 0);
+            isGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == GroundTag)
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(CollectTag)) {
+            other.enabled = false;
+            score--;
+            rem.text = "Remaining - " + score.ToString();
+            if(score == 0)
+            {
+                win.gameObject.SetActive(true);
+            }
+        }
+    }
+}
